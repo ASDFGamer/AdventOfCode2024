14733ff2e8fde7945c4a8efb6f8451ea642be30b
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-12-02 11:24:45 +0100

    Support test files

--- src/main/java/de/asdfgamer/aoc2024/AbstractDay.java
+++ src/main/java/de/asdfgamer/aoc2024/AbstractDay.java
@@ -12,11 +12,21 @@ import java.util.Objects;
 
 public abstract class AbstractDay implements Day {
 
+    private static boolean testing = false;
+
+    public static void setTesting(boolean b) {
+        AbstractDay.testing = b;
+    }
+
     protected List<String> getInput(){
         String[] nameElements = this.getClass().getName().split("\\.");
         String day = nameElements[nameElements.length-1].toLowerCase();
+        String filename = "/inputs/"+day;
+        if (testing){
+            filename = "/test/"+ day;
+        }
         try {
-            return Files.readAllLines(Paths.get(Objects.requireNonNull(getClass().getResource("/inputs/"+ day)).toURI()), StandardCharsets.UTF_8);
+            return Files.readAllLines(Paths.get(Objects.requireNonNull(getClass().getResource(filename)).toURI()), StandardCharsets.UTF_8);
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
