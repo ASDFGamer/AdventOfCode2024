b8006df8f7a4caba6d4114b51da40e2873e1ebe4
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-12-03 17:54:39 +0100

    add day 3 in Kotlin
    Part 2 doesn't work yet, but i have not a single clue what the problem could be.
    The only fix that a know of is to rewrite it with a different algorithm, and see the differences

---
+++ src/main/java/de/asdfgamer/aoc2024/Day3.kt
@@ -0,0 +1,47 @@
+package de.asdfgamer.aoc2024
+
+class Day3() : AbstractDay() {
+    override fun part1(): String {
+        var result = 0;
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        for (line in input) {
+            val results =
+                regex.findAll(line).map {
+                    it.groups[1]!!.value.toInt() * it.groups[2]!!.value.toInt()
+                }.sum();
+            result += results;
+        }
+        return result.toString();
+    }
+
+    override fun part2(): String {
+        var result = 0;
+        val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        val doRegex = Regex("^|do\\(\\)")
+        val dontRegex = Regex("don't\\(\\)")
+        for (line in input) {
+            val doStarts = doRegex.findAll(line).map { it.range.first }
+            val dontStarts = dontRegex.findAll(line).map { it.range.first }
+            val results =
+                mulRegex.findAll(line).map { matchResult ->
+                    val start = matchResult.range.first;
+                    val lastDo = doStarts.filter { it <= start }.last();
+                    val lastDont: Int = try {
+                        dontStarts.filter { it < start }.last();
+                    } catch (e: NoSuchElementException) {
+                        -1;
+                    }
+                    print("pos: $start,  lastDo: $lastDo, lastDont: $lastDont")
+                    if (lastDo > lastDont) {
+                        println(" mul")
+                        return@map matchResult.groups[1]!!.value.toInt() * matchResult.groups[2]!!.value.toInt()
+                    } else {
+                        println()
+                        return@map 0
+                    }
+                }.sum();
+            result += results;
+        }
+        return result.toString();
+    }
+}
