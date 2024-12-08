16c876ea9c16373eb673b44a51676b55190a2cba
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-12-08 21:18:11 +0100

    Fix Day 3 Part 2

--- src/main/java/de/asdfgamer/aoc2024/Day3.kt
+++ src/main/java/de/asdfgamer/aoc2024/Day3.kt
@@ -1,21 +1,45 @@
 package de.asdfgamer.aoc2024
 
-class Day3() : AbstractDay() {
+class Day3 : AbstractDay() {
     override fun part1(): String {
-        var result = 0;
+        var result = 0
         val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
         for (line in input) {
             val results =
                 regex.findAll(line).map {
                     it.groups[1]!!.value.toInt() * it.groups[2]!!.value.toInt()
-                }.sum();
-            result += results;
+                }.sum()
+            result += results
         }
-        return result.toString();
+        return result.toString()
     }
 
     override fun part2(): String {
-        var result = 0;
+        var result = 0
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
+        var enabled = true
+        for (line in input) {
+            val results =
+                regex.findAll(line).map { matchResult ->
+                    if (matchResult.value == "do()") {
+                        enabled = true
+                    } else if (matchResult.value == "don't()") {
+                        enabled = false
+                    } else if (enabled) {
+                        //println(" mul")
+                        return@map matchResult.groups[1]!!.value.toInt() * matchResult.groups[2]!!.value.toInt()
+                    }
+                    return@map 0
+                }.sum()
+            result += results
+        }
+        return result.toString()
+
+    }
+
+
+    fun part2NotWorking(): String {
+        var result = 0
         val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
         val doRegex = Regex("^|do\\(\\)")
         val dontRegex = Regex("don't\\(\\)")
@@ -24,12 +48,12 @@ class Day3() : AbstractDay() {
             val dontStarts = dontRegex.findAll(line).map { it.range.first }
             val results =
                 mulRegex.findAll(line).map { matchResult ->
-                    val start = matchResult.range.first;
-                    val lastDo = doStarts.filter { it <= start }.last();
+                    val start = matchResult.range.first
+                    val lastDo = doStarts.filter { it <= start }.last()
                     val lastDont: Int = try {
-                        dontStarts.filter { it < start }.last();
+                        dontStarts.filter { it < start }.last()
                     } catch (e: NoSuchElementException) {
-                        -1;
+                        -1
                     }
                     print("pos: $start,  lastDo: $lastDo, lastDont: $lastDont")
                     if (lastDo > lastDont) {
@@ -39,9 +63,9 @@ class Day3() : AbstractDay() {
                         println()
                         return@map 0
                     }
-                }.sum();
-            result += results;
+                }.sum()
+            result += results
         }
-        return result.toString();
+        return result.toString()
     }
 }
