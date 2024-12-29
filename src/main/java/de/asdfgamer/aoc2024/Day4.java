554ce8555b219a43f805315865bfc63e2b6add48
Christoph Wildhagen <git@christoph-wildhagen.de>
2024-12-29 21:05:05 +0100

    Add Day4 Part 2

--- src/main/java/de/asdfgamer/aoc2024/Day4.java
+++ src/main/java/de/asdfgamer/aoc2024/Day4.java
@@ -53,43 +53,29 @@ public class Day4 extends AbstractDay {
     public String part2() {
         int count = 0;
         List<String> input = getInput();
-        char[][] inputArray = input.stream().map(String::toCharArray).toArray(char[][]::new);
+        List<char[]> tempArray = input.stream().map(String::toCharArray).toList();
+        char[][] inputArray = tempArray.toArray(char[][]::new);
         for (int i = 1; i < inputArray.length - 1; i++) {
-            for (int j = 1; j < inputArray[i].length - 1; j++) {
-                count += findWordX(inputArray, i, j, 1, 0, 0, 1);
-                count += findWordX(inputArray, i, j, 1, 0, 0, -1);
-                count += findWordX(inputArray, i, j, -1, 0, 0, 1);
-                count += findWordX(inputArray, i, j, -1, 0, 0, -1);
-                count += findWordX(inputArray, i, j, 1, 1, 1, -1);
-                count += findWordX(inputArray, i, j, 1, 1, -1, 1);
-                count += findWordX(inputArray, i, j, -1, -1, -1, 1);
-                count += findWordX(inputArray, i, j, -1, -1, 1, -1);
+            for (int j = 1; j < inputArray[0].length - 1; j++) {
+                if (inputArray[i][j] != 'A') {
+                    continue;
+                }
+                if (inputArray[i - 1][j - 1] == 'M' && inputArray[i + 1][j + 1] == 'S') {
+                    if (inputArray[i + 1][j - 1] == 'M' && inputArray[i - 1][j + 1] == 'S') {
+                        count++;
+                    } else if (inputArray[i - 1][j + 1] == 'M' && inputArray[i + 1][j - 1] == 'S') {
+                        count++;
+                    }
+                } else if (inputArray[i + 1][j + 1] == 'M' && inputArray[i - 1][j - 1] == 'S') {
+                    if (inputArray[i + 1][j - 1] == 'M' && inputArray[i - 1][j + 1] == 'S') {
+                        count++;
+                    } else if (inputArray[i - 1][j + 1] == 'M' && inputArray[i + 1][j - 1] == 'S') {
+                        count++;
+                    }
+                }
             }
         }
         return Integer.toString(count);
-    }
 
-    private int findWordX(@NotNull char[][] inputArray, int mid_x, int mid_y, int x_dir, int y_dir, int x_dir2, int y_dir2) {
-        String searchWord = "MAS";
-        int cur_x = mid_x - x_dir;
-        int cur_y = mid_y - y_dir;
-        for (int i = 0; i < searchWord.length(); i++) {
-            if (inputArray[cur_x][cur_y] != searchWord.charAt(i)) {
-                return 0;
-            }
-            cur_x += x_dir;
-            cur_y += y_dir;
-        }
-        cur_x = mid_x - x_dir2;
-        cur_y = mid_y - y_dir2;
-        for (int i = 0; i < searchWord.length(); i++) {
-            if (inputArray[cur_x][cur_y] != searchWord.charAt(i)) {
-                return 0;
-            }
-            cur_x += x_dir2;
-            cur_y += y_dir2;
-        }
-        //System.out.println("Found word at " + "(" + x_dir + "," + y_dir + "), (" + x_dir2 + "," + y_dir2 + ")");
-        return 1;
     }
 }
